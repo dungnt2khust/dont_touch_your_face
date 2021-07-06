2a25ebd13ebc74b1abe6dc8090c45bb35ff86a87
Dung <dunggamo2000@gmail.com>
2021-07-06 22:48:42 +0700

    add sound and set up camera

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React, {useEffect, useRef} from 'react';
 import './App.css';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
+import {Howl} from 'howler';
+// import soundURL from './assets/hey_sondn.mp3';
+
+// var sound = new Howl({
+//   src: soundURL
+// }) ;
+
+// sound.play();
 
 function App() {
+  const video = useRef();
+  const moduleMobilenet = useRef();
+  const classifier = useRef();
+  const init = async () => {
+    console.log('init...');
+    await setupCamera(); 
+    console.log('setup camera success !');
+
+    // classifier.current = await mobilenet.load();
+    // moduleMobilenet.current = knnClassifier.create();
+
+    console.log('setup done');
+    console.log(' Không chạm tay lên mặt và bấm train 1');
+  }
+
+  const setupCamera = () => {
+    return new Promise((resolve, reject) => {
+      navigator.getUserMedia = navigator.getUserMedia || 
+      navigator.webkitGetUserMedia || 
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia;
+
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia(
+          { video: true },
+          stream => {
+            video.current.srcObject = stream;
+            video.current.addEventListener('loadeddata', resolve);
+          },
+          error => reject(error)
+        );
+      } else {
+        reject();
+      }
+    });
+  }
+
+  useEffect(() => {
+    init();
+
+    //clean up
+    return () => {
+
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <video
+        ref={video}
+        className="video"
+        autoPlay
+      />
+      <div className="control">
+        <button className="btn">Train 1</button>
+        <button className="btn">Train 2</button>
+        <button className="btn">Run</button>
+      </div>
     </div>
   );
 }
